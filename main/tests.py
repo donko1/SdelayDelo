f68bf36b5d5b029dff1b403b9e523871ff5ad6a1
donko1 <zalbertic@mail.ru>
2024-09-15 00:21:04 +0300

    Addinf tests, requirements.txt

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+class HelloWorldViewTests(TestCase):
+
+    def test_hello_world_view(self):
+        response = self.client.get(reverse('hello_world'))
+        
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertContains(response, "Hello world!")
--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import hello_world
 
 urlpatterns = [
-	path("", hello_world)
+	path("hello_world", hello_world, name="hello_world")
 ]
