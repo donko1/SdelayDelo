259543284fb2a8d729cfdf7a2bb10bd0598a9a16
donko1 <zalbertic@mail.ru>
2024-09-15 13:38:03 +0300

    Adding model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    text = models.TextField()
+
+    def __str__(self):
+        return f"Note {self.id} by User {self.user_id}: {self.text[:20]}"
