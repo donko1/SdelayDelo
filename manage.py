e2e9a734d143fb2392ddb2459992aa1ff03a290e
donko1 <zalbertic@mail.ru>
2025-02-13 19:30:31 +0300

    Modifing logging with writing that servers/tests are running

--- manage.py
+++ manage.py
@@ -6,17 +6,28 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SdelayDelo.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SdelayDelo.settings")
     try:
         from django.core.management import execute_from_command_line
+        from django.conf import settings
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    try:
+        with open(settings.LOG_FILE, "a") as f:
+            f.write(
+                f"\n### STARTING {'TESTING' if settings.TESTING else "SERVER"} ###\n\n"
+            )
+        execute_from_command_line(sys.argv)
+    finally:
+        with open(settings.LOG_FILE, "a") as f:
+            f.write(
+                f"\n### ENDING {'TESTING' if settings.TESTING else "SERVER"} ###\n\n"
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
